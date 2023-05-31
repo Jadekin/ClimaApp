be4341ea460fcda01a6952b81b90d4e0f449ab98
Karen Pinzás Morrongiello <karen.pinzasmorrongiello@freshbooks.com>
2023-05-31 14:47:47 -0500

    Update README.md

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 
 function App() {
+  const [city, setCity] = useState('Lima');
+  const [temperature, setTemperature] = useState(28);
+  const [feelsLikeTemperature, setFeelsLikeTemperature] = useState(32);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={style.wrapper}>
       <View style={style.container}>
-        <Text>Current weather</Text>
+        <Text>Current weather in {city}</Text>
+        <Text style={style.temp}>{temperature}</Text>
+        <Text style={style.feelsTemp}>Feels like {feelsLikeTemperature}</Text>
+        <Text>High: 28 | Low: 10</Text>
       </View>
     </SafeAreaView>
   );
@@ -21,6 +21,19 @@ function App() {
 const style = StyleSheet.create({
   container: {
     backgroundColor: 'pink',
+    flex: 1,
+    alignItems: 'center',
+  },
+  wrapper: {
+    flex: 1,
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+  feelsTemp: {
+    fontSize: 20,
   },
 });
+
 export default App;
